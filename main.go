326bf3be5675b8c15d94be759c92e1e09fb10dff
José <slopes.ricardo@gmail.com>
2023-03-27 21:04:45 -0300

    Programa principal

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/exec"
 )
 
-const chances = 3
-
 func main() {
+	limparTela()
+	ExibirApresentacao()
 	numeroSecreto := gerarNumero()
 
 	var chute int
 	var tentativa int
+	quantidadeTotalDeTentativa := solicitarNivelDeDificuldade()
 
-	for tentativa = 1; tentativa <= chances; tentativa++ {
+	limparTela()
+
+	for tentativa = 1; tentativa <= quantidadeTotalDeTentativa; tentativa++ {
 		chute = solicitarChute()
 		acertou := testarChute(chute, numeroSecreto)
 
@@ -22,8 +27,44 @@ func main() {
 			break
 		} else {
 			exibirMensagemErro(tentativa)
+			mostrarDica(numeroSecreto, chute)
 		}
 	}
+}
+
+func ExibirApresentacao() {
+	fmt.Println("*******************************************")
+	fmt.Println("*     Bem Vindo ao Jogo da Advinhação     *")
+	fmt.Println("*******************************************")
+	fmt.Println()
+}
+
+func solicitarNivelDeDificuldade() int {
+	var opcao int
+	fmt.Println("Qual o nível de dificuldade deseja jogar?")
+	fmt.Println("1 - Três tentativas")
+	fmt.Println("2 - Cinco tentativas")
+	fmt.Println("3 - Sete tentativas")
+	fmt.Scan(&opcao)
+
+	var tentativas int
+	switch opcao {
+	case 1:
+		tentativas = 3
+	case 2:
+		tentativas = 5
+	case 3:
+		tentativas = 7
+	default:
+		tentativas = 5
+	}
+	return tentativas
+}
+
+func limparTela() {
+	cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
 
 func gerarNumero() int {
@@ -46,9 +87,17 @@ func testarChute(chute int, numeroSecreto int) bool {
 }
 
 func exibirMensagemAcerto() {
-	fmt.Println("Parabéns, você acertou.")
+	fmt.Printf("Parabéns, você acertou.\n")
 }
 
 func exibirMensagemErro(tentativa int) {
-	fmt.Println("Você errou. Essa é a sua tentativa ", tentativa)
+	fmt.Println("Você errou. Essa é a sua tentativa número", tentativa)
+}
+
+func mostrarDica(numeroSecreto int, chute int) {
+	if chute > numeroSecreto {
+		fmt.Printf("Seu chute foi maior que o número secreto.\n\n")
+	} else {
+		fmt.Printf("Seu chute foi menor que o número secreto.\n\n")
+	}
 }
